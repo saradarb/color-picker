0f1b5d68085f342eba14c4293e9bd49065079b7a
sara <saradernouni@gmail.com>
2025-01-15 11:30:17 -0500

    dc

--- Color Picker/src/App.jsx
+++ Color Picker/src/App.jsx
@@ -4,6 +4,7 @@ import MyComponent from './MyComponent'
 import Mycomp from './Mycomp'
 import ToDo from './ToDo'
 import Todo2 from './Todo2'
+import Effects from './Effects'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
    <>
   
    
-   <Todo2 />
+   <Effects />
    
    
    </>
--- Color Picker/src/Todo2.jsx
+++ Color Picker/src/Todo2.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Todo2() {
+
+
+    const [tasks, setTasks] = useState([]);
+    const [newTasks, setNewTasks] = useState("");
+
+
+
+    function handleTasksinput(event){
+        setNewTasks(event.target.value);
+    }
+
+    function addTasks() {
+        if (newTasks.trim() !== "") {
+            setTasks((t) => [...t, newTasks]);
+            setNewTasks(""); 
+        }
+    }
+        
+    
+
+    function removeTasks(index){
+        setTasks(tasks.filter((_,i) => i !== index))
+    }
+    function moveTasksUp(index){
+        if(index > 0){
+            const UpdatedTasks = [...tasks];
+            [UpdatedTasks[index], UpdatedTasks[index -1]] =
+            [UpdatedTasks[index -1], UpdatedTasks[index]];
+            setTasks(UpdatedTasks);
+        }
+
+    }
+    function moveTasksDown(index){
+        if(index < tasks.length -1){
+            const UpdatedTasks = [...tasks];
+            [UpdatedTasks[index +1], UpdatedTasks[index]] =
+            [UpdatedTasks[index], UpdatedTasks[index +1]];
+            setTasks(UpdatedTasks);
+        }
+
+    }
+
+
+
+
+
   return (
-    <div>Todo2</div>
+    <div> 
+        <h2> To Do List</h2>
+        <input type="text" placeholder="Entre Task Here.." value={newTasks} onChange={handleTasksinput} />
+        <button onClick={addTasks}> Entre</button>
+        <ol> 
+            {tasks.map((task, index) =>
+            <li key={index}>
+                <span> {task} </span>
+                <button onClick={() => removeTasks(index)}> Delete </button>
+                <button onClick={() => moveTasksUp(index)}> Up </button>
+                <button onClick={() => moveTasksDown(index)}> Down </button>
+            </li>
+            
+            
+            )}
+            
+        </ol>
+
+
+
+
+    </div>
   )
 }
 
